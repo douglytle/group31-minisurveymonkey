88dccaf71d73cbef3a74b2a7bb41de99b9f9871a
ewang <ericwang5@cmail.carleton.ca>
2024-11-10 01:12:27 -0500

    continued survey creation page

--- src/main/java/group/thirtyone/controllers/CreateSurveyController.java
+++ src/main/java/group/thirtyone/controllers/CreateSurveyController.java
@@ -1,14 +1,27 @@
 package group.thirtyone.controllers;
 
+import group.thirtyone.surveycomponents.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CreateSurveyController {
 
     @GetMapping("/create")
     public String createSurvey(Model model) {
+        List<MultipleChoice> MCQuestions = new ArrayList<>();
+        MCQuestions.add(new MultipleChoice());
+        List<NumberRange> NRQuestions = new ArrayList<>();
+        NRQuestions.add(new NumberRange());
+        List<OpenEnded> OEQuestions = new ArrayList<>();
+        OEQuestions.add(new OpenEnded());
+        model.addAttribute("MCQuestions", MCQuestions);
+        model.addAttribute("NRQuestions", NRQuestions);
+        model.addAttribute("OEQuestions", OEQuestions);
         return "create";
     }
 }
--- src/main/resources/static/createSurvey.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    console.log("loaded");
-    var divs = document.getElementsByTagName("div");
-    for (var i = 0; i < divs.length; i++) {
-        divs[i].style.display = 'none';
-    }
-});
