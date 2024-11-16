c6cbeab60e2af65a958b3bd9cd781189ae91bada
douglytle <owen1447@rocketmail.com>
2024-11-16 14:22:33 -0500

    User Login status in now stored

--- src/main/java/group/thirtyone/controllers/UserAccountController.java
+++ src/main/java/group/thirtyone/controllers/UserAccountController.java
@@ -2,6 +2,7 @@ package group.thirtyone.controllers;
 
 import group.thirtyone.othercomponents.UserAccount;
 import group.thirtyone.persistencerepositories.UserAccountRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,18 +19,20 @@ public class UserAccountController {
     UserAccountRepository userAccountRepository;
 
     @GetMapping("/login")
-    public String viewLogin(Model model) {
+    public String viewLogin(Model model, HttpSession session) {
         model.addAttribute("userInfo", new UserAccount());
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute UserAccount userInfo, Model model) {
+    public String login(@ModelAttribute UserAccount userInfo, Model model, HttpSession session) {
 
         Iterable<UserAccount> userAccountIterable = userAccountRepository.findAll();
         for (UserAccount user : userAccountIterable) {
             if (user.getUsername().equals(userInfo.getUsername())) {
                 if (user.getPassword().equals(userInfo.getPassword())) {
+                    session.setAttribute("loggedIn", true);
+                    session.setAttribute("username", userInfo.getUsername());
                     model.addAttribute("userInfo", userInfo);
                     return "login_success";
                 }
@@ -39,13 +42,13 @@ public class UserAccountController {
     }
 
     @GetMapping("/create_account")
-    public String viewCreateAccount(Model model) {
+    public String viewCreateAccount(Model model, HttpSession session) {
         model.addAttribute("userAccount", new UserAccount());
         return "create_account";
     }
 
     @PostMapping("/create_account")
-    public String createAccount(@ModelAttribute UserAccount userAccount, Model model) {
+    public String createAccount(@ModelAttribute UserAccount userAccount, Model model, HttpSession session) {
 
         Iterable<UserAccount> userAccountIterable = userAccountRepository.findAll();
         for (UserAccount user : userAccountIterable) {
@@ -57,5 +60,11 @@ public class UserAccountController {
         model.addAttribute("userAccount", userAccount);
         userAccountRepository.save(userAccount);
         return "create_account_success";
+    }
+
+    @GetMapping("/logout")
+    public String logOut(HttpSession session) {
+        session.invalidate();
+        return "logout";
     }
 }
