b690facd0cb62e6127e7d85cc8a640315bb9a72b
Marwan Zeid <45441599+Sgt-Roukoz@users.noreply.github.com>
2024-11-16 00:58:45 -0500

    Results Page Displaying Graphs Implemented Histogram display for number range questions using CanvasJS

--- src/main/java/group/thirtyone/controllers/SurveyAndUserControllers.java
+++ src/main/java/group/thirtyone/controllers/SurveyAndUserControllers.java
@@ -2,6 +2,7 @@ package group.thirtyone.controllers;
 
 import group.thirtyone.persistencerepositories.SurveyRepository;
 import group.thirtyone.surveycomponents.MultipleChoice;
+import group.thirtyone.surveycomponents.NumberRange;
 import group.thirtyone.surveycomponents.Survey;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,6 +26,7 @@ public class SurveyAndUserControllers {
             model.addAttribute("numberrange", survey.getNumberRangeQuestions());
             model.addAttribute("openended", survey.getOpenEndedQuestions());
             model.addAttribute("multiplechoice", survey.getMultipleChoiceQuestions());
+
             ArrayList<Map<String,Integer>> mcqmaplist = new ArrayList<>();
             for (MultipleChoice mcq : survey.getMultipleChoiceQuestions()) {
                 Map<String,Integer> mcqmap = new HashMap<>();
@@ -33,8 +35,20 @@ public class SurveyAndUserControllers {
                 }
                 mcqmaplist.add(mcqmap);
             }
+
+            ArrayList<Map<String,Integer>> nrmaplist = new ArrayList<>();
+
+            for (NumberRange nr : survey.getNumberRangeQuestions()) {
+                HashSet<String> nrAnswers = new HashSet<>(nr.getAnswers());
+                Map<String,Integer> nrmap = new HashMap<>();
+                for (String choice : nrAnswers) {
+                    nrmap.put(choice, Collections.frequency(nr.getAnswers(), choice));
+                }
+                nrmaplist.add(nrmap);
+            }
+
             model.addAttribute("mcqmaplist", mcqmaplist);
-            System.out.println(mcqmaplist);
+            model.addAttribute("nrmaplist", nrmaplist);
             model.addAttribute("survey", survey);
         }
 
