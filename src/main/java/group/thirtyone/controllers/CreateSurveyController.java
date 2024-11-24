3d081e45a4d771078a0a4a3c9a13a4c46b5a060f
ewang <ericwang5@cmail.carleton.ca>
2024-11-24 17:01:29 -0500

    minor bug fixes

--- src/main/java/group/thirtyone/controllers/CreateSurveyController.java
+++ src/main/java/group/thirtyone/controllers/CreateSurveyController.java
@@ -1,7 +1,10 @@
 package group.thirtyone.controllers;
 
+import group.thirtyone.othercomponents.UserAccount;
 import group.thirtyone.persistencerepositories.SurveyRepository;
+import group.thirtyone.persistencerepositories.UserAccountRepository;
 import group.thirtyone.surveycomponents.*;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +15,11 @@ import java.util.*;
 public class CreateSurveyController {
 
     private final SurveyRepository surveyRepository;
+    private final UserAccountRepository userAccountRepository;
 
-    public CreateSurveyController(SurveyRepository surveyRepository) {
+    public CreateSurveyController(SurveyRepository surveyRepository, UserAccountRepository userAccountRepository) {
         this.surveyRepository = surveyRepository;
+        this.userAccountRepository = userAccountRepository;
     }
 
     @GetMapping("/create")
@@ -24,7 +29,8 @@ public class CreateSurveyController {
 
     @PostMapping("/createSurvey")
     @CrossOrigin
-    public String createSurveySubmit(@RequestBody String surveyJSON) {
+    public String createSurveySubmit(@RequestBody String surveyJSON, HttpSession session) {
+        //UserAccount user = userAccountRepository.findByUsername(session.getAttribute("username").toString());
         Survey survey = new Survey();
 
         String stripped = surveyJSON.replace("{","");
@@ -41,31 +47,44 @@ public class CreateSurveyController {
                 survey.setName(values[i+1].replace("value:", ""));
             }
             if(values[i].equals("name:OEQuestion")) {
-                OpenEnded oe = new OpenEnded();
-                oe.setQuestion(values[i+1].replace("value:", ""));
-                oe.setOrderOnPage(Integer.parseInt(values[i+3].replace("value:", "")));
-                questions.add(oe);
+                if(!values[i+1].replace("value:", "").isBlank()) {
+                    OpenEnded oe = new OpenEnded();
+                    oe.setQuestion(values[i+1].replace("value:", ""));
+                    oe.setOrderOnPage(Integer.parseInt(values[i+3].replace("value:", "")));
+                    questions.add(oe);
+                }
                 i+=2;
             }
             if(values[i].equals("name:NRQuestion")) {
-                NumberRange nr = new NumberRange();
-                nr.setQuestion(values[i+1].replace("value:", ""));
-                nr.setOrderOnPage(Integer.parseInt(values[i+3].replace("value:", "")));
-                nr.setMin(Integer.parseInt(values[i+5].replace("value:", "")));
-                nr.setMax(Integer.parseInt(values[i+7].replace("value:", "")));
-                questions.add(nr);
+                if(!values[i+1].replace("value:", "").isBlank()) {
+                    NumberRange nr = new NumberRange();
+                    nr.setQuestion(values[i+1].replace("value:", ""));
+                    nr.setOrderOnPage(Integer.parseInt(values[i+3].replace("value:", "")));
+                    nr.setMin(Integer.parseInt(values[i+5].replace("value:", "")));
+                    nr.setMax(Integer.parseInt(values[i+7].replace("value:", "")));
+                    questions.add(nr);
+                }
                 i+=6;
             }
             if(values[i].equals("name:MCQuestion")) {
-                MultipleChoice mc = new MultipleChoice();
-                mc.setQuestion(values[i+1].replace("value:", ""));
-                mc.setOrderOnPage(Integer.parseInt(values[i+3].replace("value:", "")));
-                i+=2;
-                while(values[i+2].equals("name:MCChoice")) {
-                    mc.addChoice(values[i+3].replace("value:", ""));
-                    i+=2;
+                if(values[i+1].replace("value:", "").isBlank()) {
+                    while(values[i+2].equals("name:MCChoice") || values[i+2].equals("name:MCOrder")) {
+                        i+=2;
+                    }
                 }
-                questions.add(mc);
+                else {
+                    MultipleChoice mc = new MultipleChoice();
+                    mc.setQuestion(values[i+1].replace("value:", ""));
+                    mc.setOrderOnPage(Integer.parseInt(values[i+3].replace("value:", "")));
+                    i+=2;
+                    while(values[i+2].equals("name:MCChoice")) {
+                        mc.addChoice(values[i+3].replace("value:", ""));
+                        i+=2;
+                    }
+                    questions.add(mc);
+                    System.out.println("Question: " + mc.getQuestion());
+                    System.out.println(mc.getQuestion().length());
+                }
             }
         }
         questions.sort(Comparator.comparing(Question::getOrderOnPage));
